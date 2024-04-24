eda7a5bdae3d54fa49dc87c5c37fabf5f93dbd97
misskluck <sigrid.b.kluck@gmail.com>
2024-04-24 10:33:09 +0200

    deleted existing js file, added new one + added first command

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let greeting = "God morgen Britt Linn!"
+console.log(greeting)
+/* 
+    let er et nøkkelord for å vise at det som kommer etterpå er en variabel, så kan du egentlig velge hva du vil som variablen, så når vi skriver = sier vi til datamaskinen at den skal lagre det som kommer etterpå, altså det som er i hermetegn 
+    vi kan bare skrive litt javascript i html-filen, ikke mye
+*/
+
