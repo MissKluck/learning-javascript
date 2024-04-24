2c78a3ad5906189e37eb7676533da8d1dd405b31
misskluck <sigrid.b.kluck@gmail.com>
2024-04-24 11:11:09 +0200

    Put in different types of js to try and see what sticks

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Definer variabel
 let greeting
+
 // Gi den en verdi
-console = "En vilkårlig verdi"
+greeting = "En vilkårlig verdi"
 
 /**
    * let er et nøkkelord for å vise at det som kommer etterpå er en variabel, så kan du egentlig velge hva du vil som variablen, så når vi skriver = sier vi til datamaskinen at den skal lagre det som kommer etterpå, altså det som er i hermetegn 
@@ -10,13 +11,45 @@ console = "En vilkårlig verdi"
 // Gjøre noe med den variabelen
 console.log(greeting)
 
+// Omdefinere verdien til variabel (gi den en ny verdi)
+greeting = "En helt ny verdi"
+console.log(greeting)
+
 // Definer konstanter
-const constandVariabel = "I am Constant"
+const constantVariabel = "I am Constant"
+
+let changeableVariable = "Testing"
 
 // Nest linje crasher programmet
-// constantVariabel = "This Does not work"
+// constantVariabel = "This does not work"
 
 /**
  * Flere linjer med
  * kommentarer
  */
+
+let notDefined
+console.log(notDefined)
+
+let notYetDefined = null
+console.log(notYetDefined)
+
+// Komplekse typer
+// Array
+const numbers = [ 1,  2,  3, 4 ]
+console.log(numbers)
+
+const names = [ 
+	«Per», 
+	«Pål», 
+	«Espen» 
+]
+console.log(names)
+
+// Objects
+const person = { 
+	email: «lars.gunnar@jobloop.no»
+	name: «Lars Gunnar»
+	age: 34
+ }
+console.log(person)
