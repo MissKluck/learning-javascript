869d66b6ed291dbd0883554556977edaaa4faa50
misskluck <sigrid.b.kluck@gmail.com>
2024-04-24 10:45:18 +0200

    broke down the js to smaller variabels for learning

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+// Definer variabel
+let greeting
+// Gi den en verdi
+console = "En vilkårlig verdi"
 
-let greeting = "God morgen Britt Linn!"
-console.log(greeting)
-/* 
-    let er et nøkkelord for å vise at det som kommer etterpå er en variabel, så kan du egentlig velge hva du vil som variablen, så når vi skriver = sier vi til datamaskinen at den skal lagre det som kommer etterpå, altså det som er i hermetegn 
-    vi kan bare skrive litt javascript i html-filen, ikke mye
+/**
+   * let er et nøkkelord for å vise at det som kommer etterpå er en variabel, så kan du egentlig velge hva du vil som variablen, så når vi skriver = sier vi til datamaskinen at den skal lagre det som kommer etterpå, altså det som er i hermetegn 
+    * vi kan bare skrive litt javascript i html-filen, ikke mye
 */
+// Gjøre noe med den variabelen
+console.log(greeting)
 
+// Definer konstanter
+const constandVariabel = "I am Constant"
+
+// Nest linje crasher programmet
+// constantVariabel = "This Does not work"
+
+/**
+ * Flere linjer med
+ * kommentarer
+ */
